72805ae9cecca717b8ee40ba5a62fd465771966a
Jo4B3 <164427876+Jo4B3@users.noreply.github.com>
2024-03-23 12:33:21 -0300

    Add files via upload

---
+++ estudos.js
@@ -0,0 +1,31 @@
+//estrutura do switch
+
+
+function alunos(nota){
+
+    //chamar o switch e indicar a ação
+    
+    switch(nota){
+    
+    //devemos por cases e indicar oq deve ser feito nela, e ao final da ação BREAK
+    
+    case 10:
+    case 9:
+    console.log("sua nota e perfeita")
+    break
+    
+    case 8:
+    case 7:
+    console.log("boa nota")
+
+    break
+    
+    //default e como se fosse o else
+    
+    default:
+    console.log("tente de novo")
+     }
+    }
+    
+    alunos(6)
+    
